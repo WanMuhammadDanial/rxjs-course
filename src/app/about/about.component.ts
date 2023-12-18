2dc5627b7baf0c6ce693dded5de173f77dd7bd7e
Wan Muhammad Danial <wmdanial12@gmail.com>
2023-12-18 22:57:16 +0800

    vid 8

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { fromEvent, interval, timer } from "rxjs";
 
 @Component({
-  selector: 'about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  selector: "about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"],
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    const interval$ = timer(3000, 1000);
 
+    const sub = interval$.subscribe((val) => console.log("stream 1 " + val));
+
+    setTimeout(() => sub.unsubscribe(), 5000);
+
+    const click$ = fromEvent(document, "click");
+
+    click$.subscribe(
+      (event) => console.log(event),
+      (err) => console.log(err),
+      () => console.log("completed")
+    );
+  }
 }
